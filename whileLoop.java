0c8050bae706a8bc2dbe0e60e0d6ca0f38c0954e
Gilbert Maredi <gilbert.maredi@sita.co.za>
2017-09-22 12:59:59 +0200

    Testing initial commit

---
+++ whileLoop.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class whileLoop
+{
+public static void main(String[] args)
+{
+//int count, number;
+//System.out.println("Enter a number");
+//Scanner keyboard = new Scanner(System.in);
+//number = keyboard.nextInt();
+//int count = 0;
+int x = 5;
+System.out.println("Enter a number" + x);
+while (x>0)
+{
+x ++;
+
+}
+
+System.out.println( x + "Buckle my shoe.");
+}
+}
